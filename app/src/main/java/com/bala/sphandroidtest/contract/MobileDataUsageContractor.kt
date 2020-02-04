52c2e526aacb3371fd20f234569863a99f4922e4
Murugesan Balasubramanian <Balasubramanian.Murugesan@liquidpay.com>
2020-02-04 10:50:32 +0800

    contract methods created

---
+++ app/src/main/java/com/bala/sphandroidtest/contract/MobileDataUsageContractor.kt
@@ -0,0 +1,30 @@
+package com.bala.sphandroidtest.contract
+
+import com.bala.sphandroidtest.models.Resources
+
+
+interface MobileDataUsageContractor {
+
+    interface DataUsageView
+    {
+        fun onResponseSuccess(data: Resources)
+        fun onResponseFailure(throwable : Throwable)
+        fun showProgressBar()
+        fun hideProgressBar()
+    }
+
+    interface DataUsagePresenter
+    {
+        fun getMobileDataUsage()
+    }
+
+    interface DataUsageModel
+    {
+        interface OnFinishedListener
+        {
+            fun onFinished(resources: Resources)
+            fun onFailure(throwable : Throwable)
+        }
+        fun getMobileDataUsage(onFinishedListener: OnFinishedListener)
+    }
+}
