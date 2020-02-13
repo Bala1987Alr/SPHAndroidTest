d59c21a76833173b6f9d4fb0b98a2fd34f8fbde1
Murugesan Balasubramanian <Balasubramanian.Murugesan@liquidpay.com>
2020-02-13 12:02:11 +0800

    Application done as per requirement

--- app/src/test/java/com/bala/sphandroidtest/ExampleUnitTest.kt
+++ app/src/test/java/com/bala/sphandroidtest/ExampleUnitTest.kt
@@ -1,17 +1,57 @@
 package com.bala.sphandroidtest
 
+import com.bala.sphandroidtest.contract.MobileDataUsageContractor
+import com.bala.sphandroidtest.presenter.MobileDataUsagePresenter
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+@RunWith(MockitoJUnitRunner::class)
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    @Mock
+    lateinit var dataUsageView: MobileDataUsageContractor.DataUsageView
+
+    @Mock
+    lateinit var resources: com.bala.sphandroidtest.models.Resources
+
+    @Mock
+    lateinit var throwable: Throwable
+
+    lateinit var mobileDataUsagePresenter: MobileDataUsagePresenter
+
+
+
+    @Before
+    fun setUp()
+    {
+        MockitoAnnotations.initMocks(this)
+        mobileDataUsagePresenter = MobileDataUsagePresenter(dataUsageView)
+
     }
+
+    @Test
+    fun testWithResource()
+    {
+        mobileDataUsagePresenter.getMobileDataUsage()
+        dataUsageView.onResponseSuccess(resources)
+        Mockito.verify(dataUsageView).onResponseSuccess(resources)
+        Mockito.verify(dataUsageView,Mockito.times(1)).onResponseSuccess(resources)
+
+    }
+
+    @Test
+    fun testWithError()
+    {
+        mobileDataUsagePresenter.getMobileDataUsage()
+        dataUsageView.onResponseFailure(throwable)
+        Mockito.verify(dataUsageView).onResponseFailure(throwable)
+
+    }
+
 }
